8be73d97a9cd299b974a31c3160634755d20788d
Ying Liu <yl1206@georgetown.edu>
2022-04-25 22:35:23 -0400

    code update

--- spam_detection.py
+++ spam_detection.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import pandas as pd 
+# import packages
+import pandas as pd
 import argparse
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
